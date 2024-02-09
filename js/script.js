c86484b11873525292dd3c48f896a948fa195ac3
jademoslares <jade.moslares@gmail.com>
2024-02-09 15:56:01 -0500

    Base game done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //variables
+const boardEls = document.getElementById("board");
+const modalEls = document.getElementById("myModal");
+const btnRestartEls = document.getElementsByClassName("btnRestart")[0];
+const gameMessageEls = document.getElementById("game-message");
 const enemies = [
   "alien1",
   "alien2",
@@ -16,20 +20,17 @@ let rows = 4;
 let columns = 5;
 let firstChoice, secondChoice;
 let cardSet; //holder of current game cards
-let time = 0;
-let guess = 5;
-let numMatchToWin = 0;
-let userMatch = 0;
+let gameTime = 60;
+let guess = 10;
 let level = 1;
-let enemiesRemaining = 0;
+let enemiesRemaining = 10;
+let isWinner;
 
-window.onload = function () {
-  render();
-};
 function render() {
   shuffleCards();
   startGame();
   gameInfo();
+  setTimeout(gameTimeCountdown, 1500);
 }
 function shuffleCards() {
   enemiesRemaining = 10;
@@ -57,17 +58,11 @@ function startGame() {
       cardEls.classList.add("alien");
       cardEls.addEventListener("click", selectCard);
       cardEls.style.width = 100 / columns + "%";
-      cardEls.style.height = 100 / row + "%";
-      document.getElementById("board").append(cardEls);
+      cardEls.style.height = 100 / rows + "%";
+      boardEls.append(cardEls);
     }
     board.push(row);
   }
-  //const boardEls = document.getElementById("board");
-  //   boardEls.addEventListener("click",(event =>{
-  //     if(event.target.tagName ==="IMG"){
-  //         selectCard();
-  //     }
-  //   }));
   //show card for 1 second before hiding
   setTimeout(hideCards, 1000);
 }
@@ -75,7 +70,7 @@ function startGame() {
 function gameInfo() {
   let gameTopEls = document.querySelector("#top-info");
   gameTopEls.innerHTML = `<h2>Memory Train</h2>
-  <p>Level ${level} <span>Lives ${guess} Time ${time}</span></p>`;
+  <p>Level ${level} <span>Lives ${guess} Time ${gameTime}</span></p>`;
 
   let gameBottomEls = document.querySelector("#bottom-info");
 }
@@ -123,9 +118,62 @@ function update() {
     secondChoice.src = "/img/holder/holder" + x.toString() + ".png";
 
     guess -= 1;
+    gameInfo();
+  } else {
+    enemiesRemaining -= 1;
   }
-
+  checkWinner();
   firstChoice = null;
   secondChoice = null;
 }
-function gameTime(cb) {}
+function checkWinner() {
+  if (gameTime === 0) {
+    isWinner = false;
+    boardEls.classList.add("disabled");
+    gameMessageEls.innerHTML = "Times Up! Better Luck next time!";
+    modalEls.style.display = "block";
+  } else if (guess === 0) {
+    isWinner = false;
+    boardEls.classList.add("disabled");
+    gameMessageEls.innerHTML = "Nice Try! You lose!";
+    modalEls.style.display = "block";
+  } else if (enemiesRemaining === 0) {
+    isWinner = true;
+    boardEls.classList.add("disabled");
+    gameMessageEls.innerHTML = "Congratulations! You won!";
+    modalEls.style.display = "block";
+  }
+}
+btnRestartEls.onclick = function () {
+  debugger;
+  modalEls.style.display = "none";
+  boardEls.classList.remove("disabled");
+  board = [];
+  rows = 4;
+  columns = 5;
+  firstChoice =null;
+  secondChoice = null;
+  cardSet = [];
+  gameTime = 60;
+  guess = 10;
+  level = 1;
+  enemiesRemaining = 10;
+  isWinner;
+  empty(boardEls);
+  render();
+};
+function empty(element){
+  while(element.firstElementChild){
+    element.firstElementChild.remove();
+  }
+}
+function gameTimeCountdown() {
+  if (gameTime != 0) {
+    gameTime -= 1;
+    gameInfo();
+    setTimeout(gameTimeCountdown, 1000);
+  } else {
+    checkWinner();
+  }
+}
+render();
